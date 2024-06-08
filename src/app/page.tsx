603339a3053313f800d2c9ce13982420cca4a3b5
Reef <4822785+xenopeg@users.noreply.github.com>
2024-06-08 18:10:23 +0100

    Tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+const dynamic = "force-dynamic";
 import { getData } from "@/data/DataSource";
 import { Prisma } from "@prisma/client";
 
@@ -17,7 +18,7 @@ interface Day {
   };
 }
 
-function Chart({ day }: { day: Day }) {
+function Chart({ day, n }: { day: Day; n: number }) {
   const users = day.users;
   if (!users) {
     return <></>;
@@ -27,13 +28,16 @@ function Chart({ day }: { day: Day }) {
   for (const k of Object.keys(users)) {
     const colorUsers = users[k];
     if (colorUsers.length > size) size = colorUsers.length;
-    total += colorUsers.length
+    total += colorUsers.length;
   }
 
   return (
     <>
-      <div>
-      Total colors: {total}
+      <div className="mb-4">
+        <span className="text-xl mr-3">Day: {n}</span>
+        <span className="text-md text-gray-400 italic">
+          Total colors: {total}
+        </span>
       </div>
       <div
         className="grid "
@@ -41,27 +45,34 @@ function Chart({ day }: { day: Day }) {
           gridTemplateColumns: ".01fr .01fr 1fr",
         }}
       >
-        {Object.entries(users).map(([color, users], i) => (
-          <>
-            <div key={i*3} className="flex flex-col justify-center items-end">
-              <span className="">{a(day.names[color])}:</span>
-            </div>
-            <div key={i*3+1}  className="flex flex-col justify-center items-end px-1">
-                {users.length}
-            </div>
-            <div  key={i*3+2}  className="p-1 box-border">
+        {Object.entries(users)
+          .sort(([ak], [bk]) => day.names[ak].localeCompare(day.names[bk]))
+          .map(([color, users], i) => (
+            <>
               <div
-                className="rounded-md overflow-visible px-2"
-                style={{
-                  background: `rgb(${day.colors[color][0]},${day.colors[color][1]},${day.colors[color][2]})`,
-                  width: (users.length / size) * 100 + "%",
-                  height: "100%",
-                }}
+                key={i * 3}
+                className="flex flex-col justify-center items-end"
               >
+                <span className="">{a(day.names[color])}:</span>
               </div>
-            </div>
-          </>
-        ))}
+              <div
+                key={i * 3 + 1}
+                className="flex flex-col justify-center items-end px-1"
+              >
+                {users.length}
+              </div>
+              <div key={i * 3 + 2} className="p-1 box-border">
+                <div
+                  className="rounded-md overflow-visible px-2"
+                  style={{
+                    background: `rgb(${day.colors[color][0]},${day.colors[color][1]},${day.colors[color][2]})`,
+                    width: (users.length / size) * 100 + "%",
+                    height: "100%",
+                  }}
+                ></div>
+              </div>
+            </>
+          ))}
       </div>
     </>
   );
@@ -69,21 +80,54 @@ function Chart({ day }: { day: Day }) {
 
 export default async function Home() {
   const data = await getData();
-  console.log(data);
+  const weeks: Day[][] = [];
+  let curWeek;
+  for (let i = 0; i < data.length; i++) {
+    const day = data[i] as Day;
+    curWeek = Math.floor(i / 7);
+    if (!weeks[curWeek]) {
+      weeks.push([]);
+    }
+    weeks[curWeek].push(day);
+  }
+
   return (
-    <div className="container bg-violet-700/10 text-white mx-auto my-8 rounded-md p-5">
-      <div className="">
-        {data.map((day, i) => {
-          return (
-            <div key={i}>
-              <div>
-              Day: {i+1}
-              </div>
-              <Chart day={day as Day} />
-            </div>
-          );
-        })}
+    <div className="container mx-auto">
+      <div className="fixed top-0 left-0 w-screen bg-slate-700 p-1">
+        {weeks.map((week, w) => (
+          <a
+            className="text-white p-1 mr-2 rounded inline-block bg-slate-600"
+            href={`#tab-${w}`}
+          >
+            Week {w + 1}
+          </a>
+        ))}
       </div>
+      {weeks.map((week, w) => {
+        return (
+          <div className="tab-content overflow-auto" id={`tab-${w}`}>
+            {week.map((day, i) => {
+              return (
+                <div
+                  key={i}
+                  className=" bg-violet-700/10 text-white mx-auto my-8 rounded-md p-5"
+                >
+                  <div className="">
+                    <div>
+                      <Chart day={day as Day} n={w * 7 + i + 1} />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.location.hash="tab-${curWeek}"`,
+        }}
+      ></script>
     </div>
   );
 }
